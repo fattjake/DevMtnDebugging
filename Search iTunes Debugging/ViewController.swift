5269c5c58df833c8423cc6ea71256ad4c51356c2
Jake Gundersen <fattjake@gmail.com>
2016-11-11 11:18:08 -0700

    Initial Commit

---
+++ Search iTunes Debugging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search iTunes Debugging
+//
+//  Created by Jake Gundersen on 11/11/16.
+//  Copyright © 2016 Jake Gundersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
